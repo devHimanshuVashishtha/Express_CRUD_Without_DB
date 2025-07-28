830d19c9b70af80a3e59ce7343eda6d680cc456d
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-28 16:35:40 +0530

    add POST method with validation

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ app.use(express.json());
 
 const users = [];
 
+
+
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email)
+    return res.json({ message: "No name and Email Enter" });
+  const emailExit = users.find((u) => u.email === email);
+  if (emailExit)
+    return res.json({ message: "Email already exist" });
+  const newUser = {
+    id: users.length + 1,
+    name,
+    email,
+    createdAt: new Date(),
+  };
+  users.push(newUser);
+  res.status(202).json({ message: "User add Successfully" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
